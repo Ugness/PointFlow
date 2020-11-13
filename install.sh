6332fec305b6505791462f7093aff8e93f208a3d
root <wogns98@kaist.ac.kr>
2020-11-13 20:03:40 +0000

    open3d version 0.9 for my serer

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ root=`pwd`
 # Install dependecies
 pip install numpy matplotlib pillow scipy tqdm scikit-learn -y
 pip install torchdiffeq==0.0.1
+pip install open3d==0.9
 
 # Compile CUDA kernel for CD/EMD loss
 cd metrics/pytorch_structural_losses/
---
+++ scripts/shapenet_airplane_gen_demo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+python demo.py \
+    --cates airplane \
+    --resume_checkpoint pretrained_models/gen/airplane/checkpoint.pt \
+    --dims 512-512-512 \
+    --latent_dims 256-256 \
+    --use_latent_flow
+
+
